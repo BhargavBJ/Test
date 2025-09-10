dc64edd0c4bc031c7bcb15c5c72058bba4f5a9c4
Bhargav B J <140739230+BhargavBJ@users.noreply.github.com>
2025-09-10 12:35:43 +0530

    Update pc2.c

--- pc2.c
+++ pc2.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
 #include<omp.h>
-int main()
+void main()
 {
-  int n;
-  printf("Enter no. of iterations:");
-  scanf("%d",&n);
-  #pragma parallel for schedule(static,4)
-  int i;
-  for(i=0;i<n;i++)
-  {
-    int id = omp_get_thread_num();
-    printf("Thread %d: Iteration %d\n",id,i);
-  }
-  return 0;
+    int n;
+    printf("Enter the number of itreations:");
+    scanf("%d",&n);
+    #pragma omp parallel  for schedule(static,4)
+    for(int i =0;i<=n;i++)
+    {
+        int tid = omp_get_thread_num();
+        printf("Thread: %d, Iteration: %d \n",tid,i);
+    }
 }
